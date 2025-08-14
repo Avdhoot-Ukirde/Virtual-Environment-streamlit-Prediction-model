73d39d5f1e4c212ad045e5aff3b7c4998e858ab6
Avdhoot-Ukirde <avdhootukirde106@gmail.com>
2025-08-14 12:12:25 +0530

    created a salary predition model using pandas , sklearn,numpy and made a application  web  with iterface using streamlit

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title("This is a title")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+st.header("_Streamlit_ is :blue[cool] :sunglasses:")
+st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
+st.caption("This is a string that explains something above.")
+st.text("This is text\n[and more text](that's not a Markdown link).")
