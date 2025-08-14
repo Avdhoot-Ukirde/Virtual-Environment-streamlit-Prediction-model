1a1141cc05db9953b6c6bfa439cd031f077ae3d0
Avdhoot-Ukirde <avdhootukirde106@gmail.com>
2025-08-14 17:10:32 +0530

    updated app.py
    UP

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-import streamlit as st
-st.title("This is a title")
-st.title("_Streamlit_ is :blue[cool] :sunglasses:")
-st.header("_Streamlit_ is :blue[cool] :sunglasses:")
-st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
-st.caption("This is a string that explains something above.")
-st.text("This is text\n[and more text](that's not a Markdown link).")
+st.title("This a Salary Prediction Model Based on Years of Experience")
+st.caption("This ML model was desighned with a sample data obtained from kaggle. Linear Regression was the algorith used to make the model")
+st.model.jobilb.load('lr_model')
+years = st.slider("Enter the number of years of Experience", min_value=0.0,max_value=20,value=5,step=0.1)
+
