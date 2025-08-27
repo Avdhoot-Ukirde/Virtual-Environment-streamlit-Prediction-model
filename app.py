fa9768bb69b44b06ddf4d68e7c58191506202d77
Avdhoot-Ukirde <avdhootukirde106@gmail.com>
2025-08-27 23:38:33 +0530

    cosine similariy distance calculation techniques used

--- app.py
+++ app.py
@@ -25,3 +25,40 @@ def recommend(movie):
     recommended_movie_posters.append(fetch_poster(movie_id))
   return recommended_movie_names,recommended_movie_posters
 
+st.header("Movie Recommender System")
+movie_list=movies['title'].values
+selected_movie=st.selectbox("Type or select a movie from the list",movie_list)
+
+if st.button("Show Recommendations"):
+  recommended_movie_names,recommended_movie_posters=recommend(selected_movie)
+  c1,c2,c3,c4,c5,c6,c7,c8,c9,c10=st.tabs(["Movie 1","Movie 2","Movie 3","Movie 4","Movie 5","Movie 6","Movie 7","Movie 8","Movie 9","Movie 10"])
+  with c1:
+    st.text(recommended_movie_names[0])
+    st.image(recommended_movie_posters[0])
+  with c2:
+    st.text(recommended_movie_names[1])
+    st.image(recommended_movie_posters[1])
+  with c3:
+    st.text(recommended_movie_names[2])
+    st.image(recommended_movie_posters[2])
+  with c4:
+    st.text(recommended_movie_names[3])
+    st.image(recommended_movie_posters[3])
+  with c5:
+    st.text(recommended_movie_names[4])
+    st.image(recommended_movie_posters[4])
+  with c6:
+    st.text(recommended_movie_names[5])
+    st.image(recommended_movie_posters[5])
+  with c7:
+    st.text(recommended_movie_names[6])
+    st.image(recommended_movie_posters[6])
+  with c8:
+    st.text(recommended_movie_names[7])
+    st.image(recommended_movie_posters[7])
+  with c9:
+    st.text(recommended_movie_names[8])
+    st.image(recommended_movie_posters[8])
+  with c10:
+    st.text(recommended_movie_names[9])
+    st.image(recommended_movie_posters[9])
